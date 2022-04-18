4404496bbadb52c9d3cffd679c2723470e06703e
Tobias Frey <tobias@fre-y.de>
2022-04-18 15:24:41 +0200

    Repo List: Refactored GitHub repo list into own component

--- src/app/explorer/github-explorer-popular-repos.component.tsx
+++ src/app/explorer/github-explorer-popular-repos.component.tsx
@@ -1,78 +1,14 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { CommandBar, DetailsList, DetailsListLayoutMode, IColumn, Link, Selection, Spinner, SpinnerSize } from '@fluentui/react';
+import { CommandBar, Selection, Spinner, SpinnerSize } from '@fluentui/react';
 import { memoComponent } from '../../util/memo-component';
 import { GithubRepo, GithubReposResponse } from '../../api/github-repos.types';
 import { getGithubPopularRepos } from '../../api/github-repos.requests';
 import { useLocalStarredRepos } from '../../util/local-storage.hooks';
+import { GithubExplorerRepoList } from './github-explorer.repo-list.component';
 
 export interface GithubExplorerPopularReposProps {}
 
 const ONE_WEEK_IN_MILLIS = 7 * 24 * 60 * 60 * 1000;
-
-const TABLE_COLUMNS: IColumn[] = [
-    {
-        key: 'name',
-        name: 'Name',
-        fieldName: 'name',
-        minWidth: 150,
-        maxWidth: 350,
-        onRenderField(props, def) {
-            if (props == null || def == null) {
-                return null;
-            }
-
-            return (
-                <Link href={props.item.html_url} key={props.item.id}>
-                    {def(props)}
-                </Link>
-            );
-        },
-    },
-    {
-        key: 'description',
-        name: 'Description',
-        fieldName: 'description',
-        minWidth: 300,
-        maxWidth: 700,
-    },
-    {
-        key: 'language',
-        name: 'Language',
-        fieldName: 'language',
-        minWidth: 100,
-        maxWidth: 200,
-    },
-    {
-        key: 'stars',
-        name: 'Stars',
-        fieldName: 'stargazers_count',
-        minWidth: 50,
-        maxWidth: 100,
-    },
-    {
-        key: 'watchers',
-        name: 'Watchers',
-        fieldName: 'watchers_count',
-        minWidth: 50,
-        maxWidth: 100,
-    },
-    {
-        key: 'forks',
-        name: 'Forks',
-        fieldName: 'forks_count',
-        minWidth: 50,
-        maxWidth: 100,
-    },
-    {
-        key: 'is_local_starred',
-        name: 'Starred',
-        fieldName: 'isLocalStarred',
-        minWidth: 50,
-        maxWidth: 100,
-    },
-];
-
-const TABLE_STYLES = {};
 
 export const GithubExplorerPopularRepos: React.FC<GithubExplorerPopularReposProps> = memoComponent('GithubExplorerPopularRepos', () => {
     const [repos, setRepos] = useState<GithubReposResponse>();
@@ -137,13 +73,7 @@ export const GithubExplorerPopularRepos: React.FC<GithubExplorerPopularReposProp
                     },
                 ]}
             />
-            <DetailsList
-                columns={TABLE_COLUMNS}
-                items={tableItems}
-                layoutMode={DetailsListLayoutMode.justified}
-                selection={tableSelection}
-                styles={TABLE_STYLES}
-            />
+            <GithubExplorerRepoList items={tableItems} selection={tableSelection} />
         </div>
     );
 });
---
+++ src/app/explorer/github-explorer.repo-list.component.tsx
@@ -0,0 +1,86 @@
+import { DetailsList, DetailsListLayoutMode, IColumn, ISelection, Link } from '@fluentui/react';
+import React from 'react';
+import { memoComponent } from '../../util/memo-component';
+import { GithubRepo } from '../../api/github-repos.types';
+
+export interface GithubExplorerRepoListProps {
+    items: (GithubRepo & { isLocalStarred: boolean })[];
+    selection?: ISelection<GithubRepo>;
+}
+
+const TABLE_COLUMNS: IColumn[] = [
+    {
+        key: 'name',
+        name: 'Name',
+        fieldName: 'name',
+        minWidth: 150,
+        maxWidth: 350,
+        onRenderField(props, def) {
+            if (props == null || def == null) {
+                return null;
+            }
+
+            return (
+                <Link href={props.item.html_url} key={props.item.id}>
+                    {def(props)}
+                </Link>
+            );
+        },
+    },
+    {
+        key: 'description',
+        name: 'Description',
+        fieldName: 'description',
+        minWidth: 300,
+        maxWidth: 700,
+    },
+    {
+        key: 'language',
+        name: 'Language',
+        fieldName: 'language',
+        minWidth: 100,
+        maxWidth: 200,
+    },
+    {
+        key: 'stars',
+        name: 'Stars',
+        fieldName: 'stargazers_count',
+        minWidth: 50,
+        maxWidth: 100,
+    },
+    {
+        key: 'watchers',
+        name: 'Watchers',
+        fieldName: 'watchers_count',
+        minWidth: 50,
+        maxWidth: 100,
+    },
+    {
+        key: 'forks',
+        name: 'Forks',
+        fieldName: 'forks_count',
+        minWidth: 50,
+        maxWidth: 100,
+    },
+    {
+        key: 'is_local_starred',
+        name: 'Starred',
+        fieldName: 'isLocalStarred',
+        minWidth: 50,
+        maxWidth: 100,
+    },
+];
+
+const TABLE_STYLES = {};
+
+export const GithubExplorerRepoList = memoComponent('GithubExplorerRepoList', ({ items, selection }) => {
+    return (
+        <DetailsList
+            columns={TABLE_COLUMNS}
+            items={items}
+            layoutMode={DetailsListLayoutMode.justified}
+            selection={selection}
+            styles={TABLE_STYLES}
+        />
+    );
+});
