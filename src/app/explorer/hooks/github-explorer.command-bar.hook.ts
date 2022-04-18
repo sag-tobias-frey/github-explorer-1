75ad2dbe4fe0489ea8265bf10e764db764790ce9
Tobias Frey <tobias@fre-y.de>
2022-04-18 16:45:58 +0200

    Command Bar Hook: Allow hiding (un)star options globally

--- src/app/explorer/hooks/github-explorer.command-bar.hook.ts
+++ src/app/explorer/hooks/github-explorer.command-bar.hook.ts
@@ -4,39 +4,53 @@ import { renderRepoLanguage } from '../github-explorer.repo-list.component';
 import { GithubRepo } from '../../../api/github-repos.types';
 import type { useLocalStarredRepos } from '../../../util/local-storage.hooks';
 
+export interface GithubExplorerCommandBarOptions {
+    hideStar?: boolean;
+    hideUnStar?: boolean;
+}
+
+function isNonNull<T>(item: T | null | undefined): item is T {
+    return item != null;
+}
+
 export const useGithubExplorerCommandBar = (
     availableLanguages: string[],
     selectedRepos: GithubRepo[],
     { addStarredRepo, deleteStarredRepo }: ReturnType<typeof useLocalStarredRepos>['actions'],
+    options?: GithubExplorerCommandBarOptions,
 ) => {
     const [filteredLanguages, setFilteredLanguages] = useState(() => new Set<string>());
 
     const items = useMemo<ICommandBarItemProps[]>(() => {
         return [
-            {
-                key: 'star_repo',
-                text: 'Star',
-                disabled: selectedRepos?.length <= 0,
-                iconProps: {
-                    iconName: 'FavoriteStarFill',
-                },
-                onClick() {
-                    selectedRepos.forEach(addStarredRepo);
-                },
-            },
-            {
-                key: 'unstar_repo',
-                text: 'Unstar',
-                disabled: selectedRepos?.length <= 0,
-                iconProps: {
-                    iconName: 'FavoriteStar',
-                },
-                onClick() {
-                    selectedRepos.forEach(deleteStarredRepo);
-                },
-            },
-        ];
-    }, [addStarredRepo, deleteStarredRepo, selectedRepos]);
+            options?.hideStar === true
+                ? null
+                : {
+                      key: 'star_repo',
+                      text: 'Star',
+                      disabled: selectedRepos?.length <= 0,
+                      iconProps: {
+                          iconName: 'FavoriteStarFill',
+                      },
+                      onClick() {
+                          selectedRepos.forEach(addStarredRepo);
+                      },
+                  },
+            options?.hideUnStar === true
+                ? null
+                : {
+                      key: 'unstar_repo',
+                      text: 'Unstar',
+                      disabled: selectedRepos?.length <= 0,
+                      iconProps: {
+                          iconName: 'FavoriteStar',
+                      },
+                      onClick() {
+                          selectedRepos.forEach(deleteStarredRepo);
+                      },
+                  },
+        ].filter(isNonNull);
+    }, [addStarredRepo, deleteStarredRepo, options?.hideStar, options?.hideUnStar, selectedRepos]);
 
     const languageFilterItem = useMemo<ICommandBarItemProps>(
         () => ({
