aa54f469ba308c4a73564b6fd13852d6b7cd386b
Tobias Frey <tobias@fre-y.de>
2022-04-18 16:28:08 +0200

    Starred Repos: Support filtering on repo languages

--- src/app/explorer/github-explorer-starred-repos.component.tsx
+++ src/app/explorer/github-explorer-starred-repos.component.tsx
@@ -1,12 +1,22 @@
 import React, { useMemo } from 'react';
+import { CommandBar } from '@fluentui/react';
 import { memoComponent } from '../../util/memo-component';
 import { GithubExplorerRepoList } from './github-explorer.repo-list.component';
 import { useLocalStarredRepos } from '../../util/local-storage.hooks';
+import { useGithubExplorerCommandBar } from './github-explorer.command-bar.hook';
 
 export interface GithubExplorerStarredReposProps {}
 
 export const GithubExplorerStarredRepos: React.FC<GithubExplorerStarredReposProps> = memoComponent('GithubExplorerStarredRepos', () => {
-    const { starredRepos } = useLocalStarredRepos();
+    const { actions, starredRepos } = useLocalStarredRepos();
+    const availableLanguages = useMemo(() => {
+        return Array.from(new Set(starredRepos.map((item) => item.language)));
+    }, [starredRepos]);
+
+    const {
+        filteredLanguages,
+        barProps: { farItems },
+    } = useGithubExplorerCommandBar(availableLanguages, [], actions);
 
     const tableItems = useMemo(() => {
         if (starredRepos == null) {
@@ -14,9 +24,15 @@ export const GithubExplorerStarredRepos: React.FC<GithubExplorerStarredReposProp
         }
 
         return starredRepos
+            .filter((repo) => filteredLanguages.size === 0 || filteredLanguages.has(repo.language))
             .sort((repo1, repo2) => repo2.stargazers_count - repo1.stargazers_count)
             .map((repo) => ({ ...repo, isLocalStarred: true }));
-    }, [starredRepos]);
+    }, [filteredLanguages, starredRepos]);
 
-    return <GithubExplorerRepoList items={tableItems} />;
+    return (
+        <div style={{ paddingTop: 8 }}>
+            <CommandBar items={[]} farItems={farItems} />
+            <GithubExplorerRepoList items={tableItems} />
+        </div>
+    );
 });
